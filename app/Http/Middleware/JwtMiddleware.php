96f93e59eb019e6e8322628dbc9dbe19ee73217e
Alvin <>
2020-12-07 15:50:49 +0400

    adding jwt authentication

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,36 @@
+<?
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+
+class JwtMiddleware
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $token=$request->header('Authorization');
+
+        if (!token) {
+            return response()->json([
+                "error"=> "Token not provided"
+            ], 401);
+        }
+        try {
+            $credentials = JWT::decodes($token, env('JWT_SECRET'), ['HS256']);
+        } catch (ExpiredException $e) {
+            return response()->json([
+                "error"=> "Provided token expired"
+            ], 400);
+        } catch (Exception $e) {
+            return response()->json([
+                "error"=>"An error while decoding token."
+            ], 400);
+        }
+        $user = User::find($credentials->sub);
+        $request->auth=$user;
+        return $next($request);
+    }
+}
