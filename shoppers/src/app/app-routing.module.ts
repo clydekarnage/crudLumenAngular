23cc7c10ba349cf42a8413430f933ddd00bbbbd6
Alvin <>
2020-12-07 16:53:09 +0400

    adding the components and created the routing module

--- shoppers/src/app/app-routing.module.ts
+++ shoppers/src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ItemsComponent } from './components/items/items.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent,
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},
+{
+  path: 'categories',
+  component: CategoriesComponent,
+},
+{
+  path: 'add-category',
+  component: AddCategoryComponent,
+},
+{
+  path: 'add-category/:id',
+  component: AddCategoryComponent,
+},
+{
+  path: 'categories/:id',
+  component: ItemsComponent,
+},
+{
+  path: 'categories/:id/add-item',
+  component: AddItemComponent,
+},
+{
+  path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  component: PageNotFoundComponent
+}, ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- shoppers/src/app/app.module.ts
+++ shoppers/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ItemsComponent } from './components/items/items.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent,
+    CategoriesComponent,
+    ItemsComponent,
+    AddCategoryComponent,
+    AddItemComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
